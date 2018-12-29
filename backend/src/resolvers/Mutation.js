d507aded5f6ca0af78dd0019047f0034fae83cad
Zeeshan Syed <zsyed89@gmail.com>
2018-12-28 23:11:39 -0500

    Created local DB, createServer, Mutations and Query

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,9 @@
-const mutations = {};
+const Mutations = {
+    createDog(parent, args, ctx, info) {
+        // Create a dog!
+        const newDog = { name: args.name };
+        return newDog;
+    }
+};
 
-module.exports = mutations;
+module.exports = Mutations;
